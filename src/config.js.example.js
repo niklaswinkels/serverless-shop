66884828d47f7843a7102c5948a03eb699834e04
Viertelkind <niklas.winkels@gmail.com>
2018-07-12 21:07:47 +0200

    config
    config

---
+++ src/config.js.example.js
@@ -0,0 +1,10 @@
+const config = {
+  stripe: {
+    apiKey: 'pk_test_RcuyF81gRAqODQ3e1HJooOpN',
+    checkoutUrl: 'https://cocky-swartz-5cef73.netlify.com/serverless/createCharge.js',
+    productsUrl: 'https://cocky-swartz-5cef73.netlify.com/serverless/fetchProducts.js',
+    currency: 'EUR',
+  },
+};
+
+export default config;
